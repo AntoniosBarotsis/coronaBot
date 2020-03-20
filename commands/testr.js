e98bfa4e6a390b90c055222aca9fbfa9040d258b
Tony <antonis.barotsis@gmail.com>
2020-03-20 21:33:06 +0200

    Renamed to testr and improved tensor memory management vastly

--- commands/testr.js
+++ commands/testr.js
@@ -1,13 +1,13 @@
 const tf = require("@tensorflow/tfjs");
 
 module.exports = {
-    name: 'test',
+    name: 'testr',
     description: 'Testing stuff',
-    usage: "``.test``",
+    usage: "``.testr``",
     execute(message, args, client) {
 
         let x_vals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let x_vals2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        let x_vals2 = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
         let y_vals = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
         let m, b;
@@ -21,7 +21,6 @@ module.exports = {
         function predict(x) {
             // Turn array into a tensor
             const xs = tf.tensor1d(x);
-
             return xs.mul(m).add(b);
         }
 
@@ -39,28 +38,28 @@ module.exports = {
 
 
         // console.log(tf.tensor1d(y_vals).print());
-        let results1 = [];
-        predict(x_vals).dataSync().forEach(e => results1.push(Math.round(e)));
         // predict(x_vals).print();
 
-        console.log(`Before training (random numbers almost): ${results1.toString()}`);
-
-        for (let i = 0; i < 10; i++) {
-            let temp = [];
-            train();
-            predict(x_vals).dataSync().forEach(e => temp.push(Math.round(e)));
-            console.log(`During Training [${i}]: ${temp.toString()}`)
-        }
+        tf.tidy(() => {
+            for (let i = 0; i < 1000; i++) {
+                let temp = [];
+                train();
+                predict(x_vals).dataSync().forEach(e => temp.push(Math.round(e)));
+            }
+        });
 
         let results2 = [];
         let results3 = [];
-        predict(x_vals).dataSync().forEach(e => results2.push(Math.round(e)));
-        predict(x_vals2).dataSync().forEach(e => results3.push(Math.round(e)));
-        // predict(x_vals).print();
+        tf.tidy(() => {
+            predict(x_vals).dataSync().forEach(e => results2.push(Math.round(e)));
+            predict(x_vals2).dataSync().forEach(e => results3.push(Math.round(e)));
+        });
 
-        console.log(`Before training (random numbers almost): ${results1.toString()}`);
-        console.log(`After training: ${results2.toString()}`);
-        console.log(`Actual ys: ${y_vals.toString()}`);
-        console.log(`After training 2: ${results3.toString()}`);
+        let str = '';
+        str +=`Given xs: ${x_vals.toString()}\n`;
+        str +=`Given ys: ${y_vals.toString()}\n`;
+        str += `Ys on same x input: ${results2.toString()}\n`;
+        message.channel.send(str + `Ys on new x input: ${results3.toString()}\n` + `Uncleaned tensors: ${tf.memory().numTensors}`);
+
     },
 };
