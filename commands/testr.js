a5d4fccb404dcbb3fc6f55ebf85412aadb793f64
Tony <antonis.barotsis@gmail.com>
2020-04-05 14:00:56 +0300

    Testing

--- commands/testr.js
+++ commands/testr.js
@@ -8,17 +8,37 @@ module.exports = {
     execute(message, args, client) {
     // import ExponentialRegression from 'ml-regression-exponential';
 
+        /*
+        A/(B+e^(C t))
+        a+bx + c(x^2)/2 + d(x^3)/6
+         */
         const valuesY = [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 20, 62, 155, 229, 322, 453, 655, 888, 1128, 1694, 2036,
-            2502, 3089, 3858, 4636, 5883, 7375, 9172, 10149, 12462, 12462, 17660, 21157, 24747, 27980, 31506, 35713, 41035];
+            2502, 3089, 3858, 4636, 5883, 7375, 9172, 10149, 12462, 12462, 17660, 21157, 24747, 27980, 31506, 35713, 41035, 47021, 53578, 59138, 63927, 69176, 74386,
+            80589, 86498, 92472, 97689, 101739, 105792, 110574, 115242, 119827];
 
         const valuesX = [];
-        for (let i = 0; i < 58; i++) { valuesX.push(i); }
+        for (let i = 0; i < valuesY.length; i++) { valuesX.push(i); }
 
         const degree = 5;
 
         const regression = new PolynomialRegression(valuesX, valuesY, degree);
 
-        message.channel.send(Math.round(regression._predict(58)));
-        message.channel.send(Math.round(regression._predict(59)));
+        // message.channel.send(`Last known value\n**Guess: ${Math.round(regression._predict(valuesY.length - 1))}**\n
+        // **Real: ${valuesY[valuesY.length - 1]}**`);
+        //
+        // message.channel.send(`Untrained value\n**Guess: ${Math.round(regression._predict(valuesY.length))}**\n
+        // **Real: ${Math.round(regression._predict(valuesY.length))}**`);
+
+
+        let str = '';
+        for (let i = 0; i < valuesY.length; i++) {
+            let guess = Math.round(regression._predict(i));
+            let finalGuess = guess > 0 ? guess : 0;
+            str += valuesY[i] - finalGuess + '\n';
+        }
+
+        message.channel.send(str);
+
+        // Math.round(regression._predict(valuesY.length))
     },
 };
